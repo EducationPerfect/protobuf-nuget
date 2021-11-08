06e805d0259dbc7102d46cd16e7c777e6cfe7334
Alexey Raga <alexey.raga@educationperfect.com>
2021-11-08 15:38:42 +1100

    Fix package generation

--- pack/entrypoint.sh
+++ pack/entrypoint.sh
@@ -41,14 +41,21 @@ echo "  - Output path     = $OUTPUT_PATH"
 echo "  - Company         = $COMPANY"
 echo "  - Authors         = $AUTHORS"
 
+export PATH="$PATH:/root/.dotnet/tools"
+
 echo "Clean up..."
 rm -rf ./src
 
+echo "Installing GRPC tools"
+export PATH="$PATH:/root/.dotnet/tools"
+dotnet tool install --global dotnet-grpc-cli --version 0.5.0
+
 echo "Creating file 'Directory.Build.props'..."
-sed -i -e 's/{{ Company }}/$COMPANY/g' -e 's/{{ Authors }}/$AUTHORS/g'  Directory.Build.props
+cp /opt/build-tools/Directory.Build.props ./Directory.Build.props
+sed -i -e "s/{{ Company }}/$COMPANY/g" -e "s/{{ Authors }}/$AUTHORS/g"  Directory.Build.props
 
 echo "Creating $PACKAGE_NAME project..."
-dotnet new classlib --name $PACKAGE_NAME --output $SRC/$PACKAGE_NAME --framework netstandard2.0 
+dotnet new classlib --name $PACKAGE_NAME --output $SRC/$PACKAGE_NAME --framework netstandard2.0
 rm -f ./$SRC/$PACKAGE_NAME/Class1.cs
 
 echo "Adding Protobuf files..."
@@ -63,8 +70,8 @@ if [ "$found" = "found" ]; then
   done
 
   echo "Packing $PACKAGE_NAME version $PACKAGE_VERSION at $OUTPUT_PATH"...
-  dotnet pack ./$SRC/$PACKAGE_NAME/$PACKAGE_NAME.csproj -c Release -p:PackageVersion=$PACKAGE_VERSION -o $OUTPUT_PATH 
+  dotnet pack ./$SRC/$PACKAGE_NAME/$PACKAGE_NAME.csproj -c Release -p:PackageVersion=$PACKAGE_VERSION -o $OUTPUT_PATH
 else
   echo "Error: No Protobuf file found at '$PROTOBUF_FOLDER'"
   exit -1
-fi  
+fi
